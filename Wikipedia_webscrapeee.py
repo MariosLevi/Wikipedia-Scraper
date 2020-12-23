f3d5b516d1a170b80eeaacfd3221ae84072b17a3
MariosLevi <76508477+MariosLevi@users.noreply.github.com>
2020-12-22 21:49:29 -0800

    Update Wikipedia_webscrapeee.py

--- Wikipedia_webscrapeee.py
+++ Wikipedia_webscrapeee.py
@@ -15,15 +15,6 @@ page_soup = soup(page_html, "html.parser")
 #arraying all the tables on the table-list based page
 list_of_tables = page_soup.findAll("table",{"class":"wikitable"})
 
-#title of each entry
-#individual_entry_title = individual_entry.find("b").text.strip()
-
-#full (clickable) link of each entry
-#individual_entry_link = "https://en.wikipedia.org" + individual_entry.a.get("href")
-
-#description of each entry
-#individual_entry_description = individual_entry.findAll("td")[1].text.strip()
-
 #creating a csv
 filename= "wikipedia.csv"
 f = open(filename, "w", encoding="utf-8")
@@ -33,6 +24,7 @@ f.write(headers)
 
 for table_number in list_of_tables:
     table_entries = table_number.findAll("tr")
+    #loop then breaks down each table's entries even more
     for individual_entry in table_entries:
         individual_entry_title = individual_entry.find("b").text.strip()
         individual_entry_link = "https://en.wikipedia.org" + individual_entry.a.get("href")
